1ff4b8ee41c44a249d7206cb9f2ad997180a9f6f
BrianElmo <105883668+Brianelmo@users.noreply.github.com>
2023-03-20 23:45:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+//HTML ELEMENTS
+
+let ipList = document.querySelector("#ip");  
+let point = document.querySelector("#location") 
+let timezoneList = document.querySelector("#tine-zone");
+let ispList = document.querySelector("#isp"); 
+let btnSearch = document.querySelector("#arrow");   
+let ipInput = document.querySelector("#inputsearch"); 
+let form = document.querySelector("#form-search");
+
+
+
+//Map 
+
+var map = L.map('map').setView([-34.608585, -58.373255
+
+], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+ 
+
+marker = L.marker([-34.608585, -58.373255]).addTo(map);  
+
+const locationIcon = L.icon({
+  iconUrl: 'images/icon-location.svg',
+  iconSize: [35, 35],
+  iconAnchor: [15, 15]
+});
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  
+  fetch(`https://ipapi.co/${ipInput.value}/json/`)
+      .then(res => res.json())
+      .then(data => renderResults(data))
+  
+  e.target.reset();
+} 
+
+
+fetch('https://ipapi.co/json/')
+    .then(res => res.json())
+    .then(data => renderResults(data))
+
+  
+ 
+
+
+// Funcion mostrar resultados en pantalla
+
+function renderResults(data) {
+  ipList.innerHTML = data.ip;
+  point.innerHTML = data.country_name;
+  timezoneList.innerHTML = data.city;
+  ispList.innerHTML = data.org; 
+
+
+  map.setView([data.latitude, data.longitude] , 13);
+  marker.setLatLang([data.latitude , data.longitude]);
+  marker.bindPopup(`<b>${data.ip}</b>`).openPopup();
+}
+
+
+
+
+
+
+
+
